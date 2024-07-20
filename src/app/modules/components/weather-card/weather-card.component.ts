c95e355ded2c0048e8cdb3af176de7c5ebbded81
Bruno Lira <brunolira1@icloud.com>
2024-07-20 12:45:48 -0300

    Implements WeatherCard

--- src/app/modules/components/weather-card/weather-card.component.ts
+++ src/app/modules/components/weather-card/weather-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 import { WeatherDatas } from 'src/app/models/interfaces/weather.interface';
 
 @Component({
@@ -6,13 +7,12 @@ import { WeatherDatas } from 'src/app/models/interfaces/weather.interface';
   templateUrl: './weather-card.component.html',
   styleUrls: []
 })
-export class WeatherCardComponent implements OnInit {
-
-
-
+export class WeatherCardComponent  {
   @Input() weatherDatasInput!: WeatherDatas
+  // Dadaos da previsão do tempo recebidos pelo component pai (home)
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
 
-  ngOnInit(): void {
-    console.log('DADOS RECEBIDOS DO COMPONENT PAI - weather', this.weatherDatasInput)
-  }
 }
